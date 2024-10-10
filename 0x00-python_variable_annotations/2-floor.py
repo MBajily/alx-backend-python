8205cfdd62911fb76d30d01ba2c949c9c57d6bdd
Mohammed Elgaily <mbajily@gmail.com>
2024-10-10 17:11:42 +0000

    tasks 1-4

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for getting the floor of a float number with type annotations."""
+
+
+def floor(n: float) -> int:
+    """Get the floor of a float number.
+
+    Args:
+        n (float): The float number
+
+    Returns:
+        int: The floor of the float number
+    """
+    return int(n)
